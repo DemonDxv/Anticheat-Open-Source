ff9dd9ba57a688f54f75ec644cef8f33ae91de13
DemonDxv <55443927+DemonDxv@users.noreply.github.com>
2021-07-10 03:16:47 -0500

    Added Spigot Update Checker

--- src/main/java/me/rhys/anticheat/Anticheat.java
+++ src/main/java/me/rhys/anticheat/Anticheat.java
@@ -11,12 +11,15 @@ import me.rhys.anticheat.config.ConfigValues;
 import me.rhys.anticheat.tinyprotocol.api.ProtocolVersion;
 import me.rhys.anticheat.tinyprotocol.api.TinyProtocolHandler;
 import me.rhys.anticheat.util.TPSUtil;
+import me.rhys.anticheat.util.UpdateChecker;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 @Getter
 public class Anticheat extends JavaPlugin {
@@ -24,6 +27,9 @@ public class Anticheat extends JavaPlugin {
 
     private UserManager userManager;
     private CommandManager commandManager;
+
+    private String longLine =
+            "-----------------------------------------------------------------------------------------------";
 
     private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private KeepaliveHandler keepaliveHandler;
@@ -61,6 +67,22 @@ public class Anticheat extends JavaPlugin {
                 1L, 1L, TimeUnit.MINUTES);
 
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TPSUtil(), 100L, 1L);
+
+        new UpdateChecker(this, 93504).getVersion(version -> {
+            if (getDescription().getVersion().equalsIgnoreCase(version)) {
+                getServer().getConsoleSender().sendMessage("\n\n" + ChatColor.GREEN
+                        + "You are currently running the latest version of the anticheat! " +
+                        "[v"+getDescription().getVersion()+"]\n\n");
+            } else {
+                getServer().getConsoleSender().sendMessage("\n\n" + ChatColor.DARK_RED
+                        + "Your current update is outdated!" + ChatColor.RESET
+                        + " Version: [v" + getDescription().getVersion() + "]," + " Latest: [v" + version + "]\n\n"
+                        + longLine + "\n"
+                        + "| Please download the latest update here: https://www.spigotmc.org/resources/anticheat.93504/ |"
+                        + "\n" + longLine + "\n\n");
+            }
+        });
+
     }
 
     @Override
---
+++ src/main/java/me/rhys/anticheat/util/UpdateChecker.java
@@ -0,0 +1,40 @@
+package me.rhys.anticheat.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+/**
+ *
+ * Spigot Update Checker
+ * https://www.spigotmc.org/wiki/creating-an-update-checker-that-checks-for-updates/
+ */
+
+public class UpdateChecker {
+
+    private JavaPlugin plugin;
+    private int resourceId;
+
+    public UpdateChecker(JavaPlugin plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    public void getVersion(final Consumer<String> consumer) {
+        Bukkit.getScheduler().runTaskAsynchronously(this.plugin, () -> {
+            try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + this.resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next());
+                }
+            } catch (IOException exception) {
+                this.plugin.getLogger().info("Cannot look for updates: " + exception.getMessage());
+            }
+        });
+    }
+}
